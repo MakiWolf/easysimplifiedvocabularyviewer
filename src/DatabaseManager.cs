78f84a6a2d81a18a7353e52825d2d06d636d287f
MakiWolf <73300633+MakiWolf@users.noreply.github.com>
2023-05-30 22:10:45 +0200

    fix build warnings

--- src/DatabaseManager.cs
+++ src/DatabaseManager.cs
@@ -10,14 +10,14 @@ namespace viewer
     {
         [Key]
         public int ID { get; set; }
-        public string language1 { get; set; }
-        public string language2 { get; set; }
+        public string? language1 { get; set; }
+        public string? language2 { get; set; }
         public int mistakes { get; set; }
     }
 
     public class DatabaseManager
     {
-        public List<NAME> Getname(string l1, string l2)
+        public List<NAME>? Getname(string l1, string l2)
         {
 
             using (var connection = new SQLiteConnection(Program.t))
